22b4611354d9f0b633804efd4f1164cbb29d74dc
Autentico <94708345+Abnerluisz@users.noreply.github.com>
2022-10-24 22:22:45 -0300

    Update Renomear.py

--- Renomear.py
+++ Renomear.py
@@ -1,15 +1,15 @@
-import os
+import os #ela é uma biblioteca de comandos do sistema operacional que vai te auxiliar a fazer algumas operações dentro do seu computador
 
 
-lugar_que_vai_ser_renomeado = 'C:\\Users\\Pasta\\Desktop\\Nova Pasta'
+lugar_que_vai_ser_renomeado = 'C:\\Users\\Pasta\\Desktop\\Nova Pasta' # Variavel lugar de destino
 
 path = os.chdir (lugar_que_vai_ser_renomeado)
 
-lugarnome = os.listdir
+lugarnome = os.listdir # é usado para obter a lista de todos os arquivos e diretórios no diretório especificado.
 i = 0
 
-for file in os.listdir(path):
-    new_file_name = "Geral {}.png".format(i)
+for file in os.listdir(path): 
+    new_file_name = "RENOMEARAQUI {}.png".format(i) # Só Renomear  ali RENOMEARAQUI .png(pode ser outro formato exemplo .doc)
     os.rename(file, new_file_name)
     i = i + 1
 
@@ -19,4 +19,4 @@ for file in os.listdir(path):
     Numero_incial += 1
 
 
-print(f'\nTotal de arquivos {Numero_incial}\n\nTerminado\n')
+print(f'\nTotal de arquivos {Numero_incial}\n\nTerminado\n')  #Printando os arquivos gerais renomeados e totalizando-os eles.
